88d11f3c15070d7ab9e0377f8b382ef345c1bfd7
chenqi <9468305@qq.com>
2015-04-16 19:08:45 +0800

    add MAP_POPULATE macro

--- src/win/mman.h
+++ src/win/mman.h
@@ -33,6 +33,7 @@ extern "C" {
 #define MAP_FIXED       0x10
 #define MAP_ANONYMOUS   0x20
 #define MAP_ANON        MAP_ANONYMOUS
+#define MAP_POPULATE    0x8000
 
 #define MAP_FAILED      ((void *)-1)
 
